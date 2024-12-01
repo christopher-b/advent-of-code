5adce3af755b7291bc740b0b88c8223c987a0e9b
Christopher Bennell <cbennell@hey.com>
2024-12-01 17:22:32 -0500

    Add test data

--- test/advent/day_info.test.rb
+++ test/advent/day_info.test.rb
@@ -1,20 +1,18 @@
+day_info = Advent::DayInfo.new(2099, 1)
+
 test "creates a new day_info" do
-  day_info = DayInfo.new(year: 2024, day: 1)
-  assert day_info.year == 2024
+  assert day_info.year == 2099
   assert day_info.day == 1
 end
 
 test "returns the challenge class" do
-  day_info = DayInfo.new(year: 2024, day: 1)
-  assert day_info.challenge_class == Advent::Year2024::Day01
+  assert day_info.challenge_class == Advent::Year2099::Day01
 end
 
 test "returns the sample data" do
-  day_info = DayInfo.new(year: 2024, day: 1)
   assert File === day_info.sample_data
 end
 
 test "returns the data" do
-  day_info = DayInfo.new(year: 2024, day: 1)
   assert File === day_info.data
 end
