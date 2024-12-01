11be83814e4a94c835d5fb27b132b92ab64bd33b
Christopher Bennell <cbennell@hey.com>
2024-12-01 01:57:08 -0500

    Enhance Generator: more feedback, better templates

--- lib/advent/generator.rb
+++ lib/advent/generator.rb
@@ -14,13 +14,13 @@ module Advent
       logger.info "Ensuring year folders exist"
       ensure_year_folders
 
-      logger.info "Generating challenge"
-      generate_challenge
+      logger.info "Generating challenge at #{day_info.challenge_file}"
+      logger.info "Challenge already exists" unless generate_challenge
 
-      logger.info "Generating test"
-      generate_test
+      logger.info "Generating test at #{day_info.test_file}"
+      logger.info "Test already exists" unless generate_test
 
-      logger.info "Generating data"
+      logger.info "Generating data files at #{day_info.data_path} and #{day_info.sample_data_path}"
       generate_data
     end
 
@@ -35,13 +35,23 @@ module Advent
     def generate_challenge
       dest = day_info.challenge_file
       challenge_contents = Templates::CHALLENGE % {year: day_info.year, day: day_info.padded_day}
-      File.write(dest, challenge_contents) unless File.exist?(dest)
+      if File.exist?(dest)
+        false
+      else
+        File.write(dest, challenge_contents)
+        true
+      end
     end
 
     def generate_test
       dest = day_info.test_file
       test_contents = Templates::TEST % {year: day_info.year, day: day_info.day}
-      File.write(dest, test_contents) unless File.exist?(dest)
+      if File.exist?(dest)
+        false
+      else
+        File.write(dest, test_contents)
+        true
+      end
     end
 
     def generate_data
@@ -56,7 +66,16 @@ module Advent
         module Year%{year}
           class Day%{day} < Advent::Challenge
             def call
-              true
+              puts "Part 1: \#{part1}"
+              puts "Part 2: \#{part2}"
+            end
+
+            def part1
+              0
+            end
+
+            def part2
+              0
             end
           end
         end
@@ -67,9 +86,12 @@ module Advent
       day_info = DayInfo.new(%{year}, %{day})
       challenge = day_info.challenge_with_sample
 
-      test "" do
-        result = challenge.call
-        assert result
+      test "part 1" do
+        assert challenge.part1 == 0
+      end
+
+      test "part 2" do
+        assert challenge.part2 == 0
       end
     TEST
   end
