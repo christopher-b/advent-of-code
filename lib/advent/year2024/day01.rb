7be9e67f8a0e8c4d65019b3ffc73f46271c61f4d
Christopher Bennell <cbennell@hey.com>
2024-12-01 01:57:59 -0500

    Day 1 solution

--- lib/advent/year2024/day01.rb
+++ lib/advent/year2024/day01.rb
@@ -1,8 +1,42 @@
 module Advent
   module Year2024
-    class Day01
+    class Day01 < Advent::Challenge
       def call
-        true
+        puts "Part 1: #{part1}"
+        puts "Part 2: #{part2}"
+      end
+
+      def part1
+        distance(*lists)
+      end
+
+      def part2
+        similarity(*lists)
+      end
+
+      def lists
+        list1 = []
+        list2 = []
+
+        lines.each do |line|
+          items = line.split
+          list1 << items[0].to_i
+          list2 << items[1].to_i
+        end
+
+        [list1.sort!, list2.sort!]
+      end
+
+      def distance(list1, list2)
+        list1.zip(list2).map { |a, b| (a - b).abs }.sum
+      end
+
+      def similarity(list1, list2)
+        score = 0
+        list1.each do |item|
+          score += item * list2.count(item)
+        end
+        score
       end
     end
   end
--- test/advent/year2024/day01.test.rb
+++ test/advent/year2024/day01.test.rb
@@ -1,6 +1,28 @@
-day = Advent::Year2024::Day01.new
+day_info = DayInfo.new(2024, 1)
+challenge = day_info.challenge_with_sample
 
-test "smoke test" do
-  result = day.call
-  assert result
+test "part 1" do
+  assert challenge.part1 == 11
+end
+
+test "part 2" do
+  assert challenge.part2 == 31
+end
+
+test "build lists" do
+  list1, _list2 = challenge.lists
+  assert list1.size == 6
+  assert list1.first == 1
+end
+
+test "calculate distance" do
+  list1 = [1, 1]
+  list2 = [2, 2]
+  assert challenge.distance(list1, list2) == 2
+end
+
+test "calculate similarity" do
+  list1 = [3]
+  list2 = [3, 3, 3]
+  assert challenge.similarity(list1, list2) == 9
 end
