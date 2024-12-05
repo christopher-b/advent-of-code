0233743339c77dfd84aa04df027fd2fa106facf1
Christopher Bennell <cbennell@hey.com>
2024-12-05 11:43:08 -0500

    Update Day 5 to use topological sort

--- test/advent/year2024/day05.test.rb
+++ test/advent/year2024/day05.test.rb
@@ -10,20 +10,10 @@ test "part 2" do
   assert challenge.part2 == 123
 end
 
-test "rules" do
-  assert challenge.rules[47].first == [47, 53]
-  assert challenge.rules[47].size == 6
+test "invalid updates" do
+  assert challenge.invalid_updates.size == 3
 end
 
-test "updates" do
-  assert challenge.updates.first == [75, 47, 61, 53, 29]
-  assert challenge.updates.size == 6
-end
-
-test "updates to print" do
-  assert challenge.correct_updates.size == 3
-end
-
-test "incorrect updates" do
-  assert challenge.incorrect_updates.size == 3
+test "tsort" do
+  assert challenge.invalid_updates.first.sort!.pages == [97, 75, 47, 61, 53]
 end
