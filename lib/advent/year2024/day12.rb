0f3aed25980031520d853193b27d2cce5447023a
Christopher Bennell <cbennell@hey.com>
2024-12-12 21:49:21 -0500

    Refactor Day 12

--- lib/advent/year2024/day12.rb
+++ lib/advent/year2024/day12.rb
@@ -8,111 +8,107 @@ module Advent
       # I'm sure the corner checking could be more elegant but, ¯\_(ツ)_/¯
 
       def part1
-        regions.sum(&:fence_cost)
+        garden.regions.sum(&:fence_cost)
       end
 
       def part2
-        regions.sum(&:fence_bulk_cost)
+        garden.regions.sum(&:fence_bulk_cost)
       end
 
-      Region = Struct.new(:perimeter, :area, :corners, :value) do
-        def fence_cost
-          perimeter * area
-        end
-
-        def fence_bulk_cost
-          corners.size * area
-        end
+      def garden
+        @gardern ||= GardenPlot.new(input_lines)
       end
 
-      def regions
-        @regions ||= [].tap do |rs|
-          map.each_point do |point, char|
-            # Recursively scan each cell that isn't part of a recongized region
-            next if visited.include?(point)
-            rs << find_region(point, char, Region.new(0, 0, Set.new, char))
-          end
-        end
-      end
-
-      def find_region(point, value, region)
-        return region if visited.include?(point)
-        visited << point
-
-        # Find adjacent points in the same region
-        adjacent_points = map.adjacent_points_in_range(point).select do |adjacent_point|
-          map.value_at(adjacent_point) == value
-        end
-
-        region.perimeter += 4 - adjacent_points.size
-        region.area += 1
-        region.corners += calculate_corners(point, value, region.corners)
-
-        # Recurse
-        adjacent_points.each do |adjacent_point|
-          find_region(adjacent_point, value, region)
-        end
-
-        region
-      end
-
-      # Returns an array of corner points for the given point
-      # We create 4 2x2 windows around the point, and check for matching values
-      def calculate_corners(point, value, existing_corners)
-        corners = []
-
-        n = Point.new(point.x, point.y - 1)
-        e = Point.new(point.x + 1, point.y)
-        s = Point.new(point.x, point.y + 1)
-        w = Point.new(point.x - 1, point.y)
-        nw = Point.new(point.x - 1, point.y - 1)
-        ne = Point.new(point.x + 1, point.y - 1)
-        se = Point.new(point.x + 1, point.y + 1)
-        sw = Point.new(point.x - 1, point.y + 1)
-
-        windows = [
-          [point, e, ne, n],
-          [point, n, nw, w],
-          [point, w, sw, s],
-          [point, s, se, e]
-        ]
-
-        windows.each do |window|
-          values = window.map do |p|
-            map.value_at(p)
-          rescue Grid::OutOfRangeError
-            nil
+      class GardenPlot < Grid
+        Region = Struct.new(:perimeter, :area, :corners, :value) do
+          def fence_cost
+            perimeter * area
           end
 
-          matching_values = values.count(value)
-          case matching_values
-          # OO
-          # OX
-          when 1 then corners << window.sort!
-          # OX or OO or XO
-          # OX    XX    OX
-          when 2
-            # If two diagonal points match, that's a corner!
-            # If two diagonal points match, and we've already found this corner in the same region, that's two corners!
-            if values[0] == values[2] # [2] is the diagonal point
-              window << Point.new(-1, -1) if existing_corners.include?(window.sort) # Add a dummy point to indicate a second corner
-              corners << window.sort!
+          def fence_bulk_cost
+            corners.size * area
+          end
+        end
+
+        def initialize(rows)
+          @visited = Set.new
+          super
+        end
+
+        def regions
+          @regions ||= [].tap do |rs|
+            each_point do |point, char|
+              # Recursively scan each cell that isn't part of a recognized region
+              next if @visited.include?(point)
+              rs << populate_region_info(point, char, Region.new(0, 0, Set.new, char))
             end
-          # XO or XX or OX
-          # XX    OX    XX
-          when 3 then corners << window.sort!
-          end # No corner if all 4 match
+          end
         end
 
-        corners
-      end
+        private
 
-      def map
-        @map ||= Grid.new(input_lines)
-      end
+        def populate_region_info(point, value, region)
+          return region if @visited.include?(point)
+          @visited << point
 
-      def visited
-        @visited ||= Set.new
+          # Find adjacent points in the same region
+          matching_adjacent_points = adjacent_points_in_range(point).select do |adjacent_point|
+            value_at(adjacent_point) == value
+          end
+
+          region.perimeter += 4 - matching_adjacent_points.size
+          region.area += 1
+          region.corners += detect_corners(point, value, region.corners)
+
+          # Recurse
+          matching_adjacent_points.each do |adjacent_point|
+            populate_region_info(adjacent_point, value, region)
+          end
+
+          region
+        end
+
+        # Returns an array of corner points for the given point
+        # We create 4 2x2 windows around the point, and check for matching values
+        # We keep detected corners in a set to avoid double counting
+        # A corner is described by the four points surrounding it
+        def detect_corners(point, value, existing_corners)
+          [].tap do |corners|
+            offsets = [
+              [[0, 0], [1, 0], [1, 1], [0, 1]],
+              [[0, 0], [0, 1], [-1, 1], [-1, 0]],
+              [[0, 0], [-1, 0], [-1, -1], [0, -1]],
+              [[0, 0], [0, -1], [1, -1], [1, 0]]
+            ]
+
+            offsets.each do |window_offsets|
+              window = window_offsets.map { |dx, dy| Point.new(point.x + dx, point.y + dy) }
+
+              values = window.map do |p|
+                value_at(p)
+              rescue Grid::OutOfRangeError
+                nil
+              end
+
+              matching_value_count = values.count(value)
+
+              case matching_value_count
+              # 1: Only the original point is left, so we have a corner
+              # 3: Some L-shape, so we have a corner
+              when 1, 3 then corners << window.sort!
+              when 2
+                # If the two matching points are adjacent (value[1] and value[3]), that's not a corner
+                # If two diagonal points match, that's a corner!
+                # If two diagonal points match, and we've already found this corner in the same region, that's two corners!
+                if values[0] == values[2] # [2] is the diagonal point
+                  window << Point.new(-1, -1) if existing_corners.include?(window.sort) # Add a dummy point to indicate a second corner. This allows a second point in the set
+                  corners << window.sort!
+                end
+              end # No corner if all 4 match
+            end
+          end
+        end
+        # End of GardenPlot
       end
     end
   end
