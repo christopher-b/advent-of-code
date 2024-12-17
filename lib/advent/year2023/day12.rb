942441d4dc7daa0a4d91d058eaabe4fd132387c5
Christopher Bennell <cbennell@hey.com>
2024-12-17 18:24:09 -0500

    2023 Day 12 scaffold

---
+++ lib/advent/year2023/day12.rb
@@ -0,0 +1,14 @@
+# https://adventofcode.com/2023/day/12
+module Advent
+  module Year2023
+    class Day12 < Advent::Challenge
+      # Explanation here
+
+      def part1
+      end
+
+      def part2
+      end
+    end
+  end
+end
---
+++ test/advent/year2023/day12.test.rb
@@ -0,0 +1,10 @@
+# https://adventofcode.com/2023/day/12
+challenge = Advent::Challenge.get_with_sample(year: 2023, day: 12)
+
+test "part 1" do
+  expect(challenge.part1) == 21
+end
+
+test "part 2" do
+  # expect(challenge.part2) == 0
+end
