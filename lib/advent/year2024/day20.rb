9d3feecde0d39e5c752f11ee3895a8a2b890964d
Christopher Bennell <cbennell@hey.com>
2024-12-21 01:11:44 -0500

    Day 20 Part 2 Solution WIP

--- lib/advent/year2024/day20.rb
+++ lib/advent/year2024/day20.rb
@@ -5,56 +5,90 @@ module Advent
       # Explanation here
 
       def part1
-        shortcut_times.select { |time| time >= 100 }.size
+        shortcut_count
       end
 
       def part2
+        # 2203962 too high
+        # Not 1101981
+        shortcut_count_big_cheats
       end
 
-      def path
-        @path ||= [].tap do |steps|
-          current_position = origin
-          steps << current_position
-
-          while current_position != finish
-            next_steps = maze.adjacent_points_in_range(current_position)
-            next_steps.each do |next_step|
-              value = maze.value_at(next_step)
-              next if value == "#" || steps.include?(next_step)
-
-              steps << next_step
-              current_position = next_step
+      # Prepopulate a grid representing the distance from the origin to each path point
+      def distance_grid
+        @distance_grid ||= Array.new(maze.height) { Array.new(maze.width, -1) }.tap do |distances|
+          current = origin
+          distances[current.y][current.x] = 0
+          while current != finish
+            maze.adjacent_points_in_range(current).each do |point|
+              next if maze.value_at(point) == "#"
+              next if distances[point.y][point.x] != -1
+              distances[point.y][point.x] = distances[current.y][current.x] + 1
+              current = point
             end
           end
         end
       end
 
-      def shortcut_times
-        @shortcut_times ||= shortcuts.map do |wall, steps|
-          # Find the distance between steps
-          path.find_index(steps.last) - path.find_index(steps.first) - 2
-        end
-      end
+      def shortcut_count_big_cheats(cutoff = 100)
+        @shortcut_count_big_cheats ||= begin
+          count = 0
+          # Iterate the whole grid
+          maze.each_point do |start_point, char|
+            next if char == "#"
 
-      def shortcuts
-        @shortcuts ||= Hash.new { |h, k| h[k] = [] }.tap do |shortcuts|
-          path.each do |step|
-            adjacent_walls = maze
-              .adjacent_points_in_range(step)
-              .select { |point| maze.value_at(point) == "#" }
-
-            adjacent_walls.each do |wall|
-              shortcuts[wall] << step
+            # Find all points within 20 of the start point
+            (2..20).each do |radius|
+              # For each radius, find all points by varying diff_row and diff_col
+              (0..radius).each do |diff_row|
+                diff_col = radius - diff_row
+                # Fid this point in all four quadrants around the start point
+                options = Set.new([
+                  start_point + Point.new(diff_col, diff_row),
+                  start_point + Point.new(diff_col, -diff_row),
+                  start_point + Point.new(-diff_col, diff_row),
+                  start_point + Point.new(-diff_col, -diff_row)
+                ])
+                options.each do |end_point|
+                  next unless maze.in_range?(end_point)
+                  next if maze.value_at(end_point) == "#"
+                  # We now have a valid shortcut. Check the distance it saves
+                  distance = distance_grid[end_point.y][end_point.x] - distance_grid[start_point.y][start_point.x]
+                  if distance >= cutoff + radius
+                    count += 1
+                  end
+                end
+              end
             end
           end
 
-          shortcuts.each do |wall, steps|
-            # Remove steps that are not in a straight line
-            x_groups = steps.group_by(&:x)
-            y_groups = steps.group_by(&:y)
-            steps.delete_if { |step| x_groups[step.x].size == 1 && y_groups[step.y].size == 1 }
+          count
+        end
+      end
+
+      def shortcut_count(cutoff = 100)
+        @shortcut_count ||= begin
+          count = 0
+          # Iterate the whole grid
+          maze.each_point do |start_point, char|
+            next if char == "#"
+
+            # Check the tile two spaces over
+            [
+              start_point + Point.new(2, 0),
+              start_point + Point.new(0, 2),
+            ].each do |end_point|
+              next unless maze.in_range?(end_point)
+              next if maze.value_at(end_point) == "#"
+
+              distance = distance_grid[end_point.y][end_point.x] - distance_grid[start_point.y][start_point.x]
+              if distance.abs >= cutoff + 2
+                count += 1
+              end
+            end
           end
-          shortcuts.delete_if { |wall, steps| steps.size < 2 }
+
+          count
         end
       end
 
--- test/advent/year2024/day20.test.rb
+++ test/advent/year2024/day20.test.rb
@@ -3,7 +3,7 @@ challenge = Advent::Challenge.get_with_sample(year: 2024, day: 20)
 Point = Advent::Point
 
 test "part 1" do
-  # assert_equal 0, challenge.part1
+  # shortcut_times is the best test for part 1
 end
 
 test "part 2" do
@@ -14,15 +14,19 @@ test "origin" do
   assert_equal Point.new(1, 3), challenge.origin
 end
 
-test "path" do
-  assert_equal 85, challenge.path.size
-end
+# test "path" do
+#   assert_equal 85, challenge.path.size
+# end
 
-test "shortcuts" do
-  assert_equal 44, challenge.shortcuts.values.size
-end
+# test "shortcuts" do
+#   assert_equal 44, challenge.shortcuts.values.size
+# end
 
 test "shortcut_times" do
-  times = [2] * 14 + [4] * 14 + [6] * 2 + [8] * 4 + [10] * 2 + [12] * 3 + [20] + [36] + [38] + [40] + [64]
-  assert_equal times, challenge.shortcut_times.sort
+  # times = [2] * 14 + [4] * 14 + [6] * 2 + [8] * 4 + [10] * 2 + [12] * 3 + [20] + [36] + [38] + [40] + [64]
+  # assert_equal times, challenge.shortcut_times.sort
+end
+
+test "distance_grid" do
+  distance_grid = challenge.distance_grid
 end
